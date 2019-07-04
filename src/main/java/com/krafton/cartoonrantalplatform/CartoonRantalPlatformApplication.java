0823e0085fcc5a83e987682f4d091abce6c0f0b1
mollder <lks6522@naver.com>
2019-07-04 19:00:26 +0900

    #1 gradle 환경 세팅

---
+++ src/main/java/com/krafton/cartoonrantalplatform/CartoonRantalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.krafton.cartoonrantalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoonRantalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoonRantalPlatformApplication.class, args);
+	}
+
+}
